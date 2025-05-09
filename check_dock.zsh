ef391d21eedab00d4b4c5ff8350ab5571ffa4d10
Fearless40 <spivack@gmail.com>
2025-05-08 22:08:20 -0700

    Updated auto GPU detection for hyprland

--- check_dock.zsh
+++ check_dock.zsh
@@ -3,18 +3,13 @@
 
 local cardCount=$(ls /dev/dri | grep card | wc -l)
 local nvidia_pc=$(lspci | grep VGA | grep NVIDIA | awk '{print $1}')
-local nvidia_card=$(ls -la /dev/dri/by-path/ | grep $nvidia_pc | grep card | awk '{sub("../","",$11); print $11}')
-local intel_pc=$(lspci | grep VGA | grep Intel | awk '{print $11}') 
-local intel_card=$(ls -la /dev/dri/by-path/ | grep $intel_pc | grep card | awk '{sub("../","",$11); print $11}')
-
-
-
-if [[ "cardCount"  -eq 1 ]] then 
-   echo "env = AQ_DRM_DEVICES,/dev/dri/$intel_card" > ~/.config/hypr/env.conf
-   # ln -sf ~/.config/hypr/env_undocked.conf ~/.config/hypr/env.conf
-   ln -sf ~/.config/ghostty/config_undocked ~/.config/ghostty/config
-else 
-   # ln -sf ~/.config/hypr/env_docked.conf ~/.config/hypr/env.conf
+if [[ -n "$nvidia_pc" ]] then 
+   local nvidia_card=$(ls -la /dev/dri/by-path/ | grep $nvidia_pc | grep card | awk '{sub("../","",$11); print $11}')
    echo "env = AQ_DRM_DEVICES,/dev/dri/$nvidia_card" > ~/.config/hypr/env.conf
    ln -sf ~/.config/ghostty/config_docked ~/.config/ghostty/config
+else
+   local intel_pc=$(lspci | grep VGA | grep Intel | awk '{print $1}') 
+   local intel_card=$(ls -la /dev/dri/by-path/ | grep $intel_pc | grep card | awk '{sub("../","",$11); print $11}')
+   echo "env = AQ_DRM_DEVICES,/dev/dri/$intel_card" > ~/.config/hypr/env.conf
+   ln -sf ~/.config/ghostty/config_undocked ~/.config/ghostty/config
 fi
